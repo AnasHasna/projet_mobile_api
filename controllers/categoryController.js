b21bf5bd207e1ef68be94b3e2ea67b6f17a22d4a
bensaltana hassan <bensaltana10@gmail.com>
2023-12-12 23:21:51 +0100

    finish category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,7 +2,10 @@ import asyncHandler from "express-async-handler";
 import fs from "fs";
 import path from "path";
 import Category from "../models/categoryModel.js";
-import { cloudinaryUploadImage } from "../utils/cloudinary.js";
+import {
+  cloudinaryRemoveImage,
+  cloudinaryUploadImage,
+} from "../utils/cloudinary.js";
 
 /**
  * @description     Get all categories
@@ -64,4 +67,71 @@ const addCategoryController = asyncHandler(async (req, res) => {
   });
 });
 
-export { addCategoryController, getCategoriesController };
+/**
+ * @description     update category
+ * @router          /categories/:id
+ * @method          PUT
+ * @access          private (admin)
+ */
+
+const updateCategoryController = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  const category = await Category.findById(req.params.id);
+  if (!category) {
+    return res.status(404).json({
+      status: "fail",
+      message: "category not found",
+    });
+  }
+  category.name = name;
+  category.description = description;
+
+  const file = req.file;
+  if (file) {
+    const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
+    const result = await cloudinaryUploadImage(imagePath);
+    await cloudinaryRemoveImage(category.image.publicId);
+    category.image = {
+      url: result.url,
+      publicId: result.public_id,
+    };
+    fs.unlinkSync(imagePath);
+  }
+
+  await category.save();
+
+  res.status(200).json({
+    status: "success",
+    data: category,
+  });
+});
+
+/**
+ * @description     delete category
+ * @router          /categories/:id
+ * @method          DELETE
+ * @access          private (admin)
+ */
+
+const deleteCategoryController = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.params.id);
+  if (!category) {
+    return res.status(404).json({
+      status: "fail",
+      message: "category not found",
+    });
+  }
+  await cloudinaryRemoveImage(category.image.publicId);
+  await category.remove();
+  res.status(200).json({
+    status: "success",
+    message: "category deleted",
+  });
+});
+
+export {
+  addCategoryController,
+  deleteCategoryController,
+  getCategoriesController,
+  updateCategoryController,
+};
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,14 +1,25 @@
 import express from "express";
 import {
   addCategoryController,
+  deleteCategoryController,
   getCategoriesController,
+  updateCategoryController,
 } from "../controllers/categoryController.js";
+import {
+  verifyToken,
+  verifyTokenAndBeAdmin,
+} from "../middleware/verifyToken.js";
 
 const categoryRouter = express.Router();
 
 categoryRouter
   .route("/")
-  .get(getCategoriesController)
-  .post(addCategoryController);
+  .get(verifyToken, getCategoriesController)
+  .post(verifyTokenAndBeAdmin, addCategoryController);
+
+categoryRouter
+  .route("/:id")
+  .get(verifyTokenAndBeAdmin, updateCategoryController)
+  .delete(verifyTokenAndBeAdmin, deleteCategoryController);
 
 export default categoryRouter;
